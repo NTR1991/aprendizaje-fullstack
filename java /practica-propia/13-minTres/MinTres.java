10f764f1d96d4b0dc910d506fb9e9bcd511a909b
NTRJ91 <reyesjimeneznestor@gmail.com>
2026-06-05 12:47:01 +0200

    Create MinTres.java

---
+++ java /practica-propia/13-minTres/MinTres.java
@@ -0,0 +1,11 @@
+public class MinTres {
+    public static int minTres(int a, int b, int c) {
+        if (a <= b && a <= c) {
+            return a;
+        }
+        if (b <= a && b <= c) {
+            return b;
+        }
+        return c;
+    }
+}
