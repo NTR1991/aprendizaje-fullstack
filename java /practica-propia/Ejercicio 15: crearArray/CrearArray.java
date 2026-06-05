91c06baa7f9c4fdbbceb652e8746dc55f81ad7cf
NTRJ91 <reyesjimeneznestor@gmail.com>
2026-06-05 13:36:07 +0200

    Create CrearArray.java

---
+++ java /practica-propia/Ejercicio 15: crearArray/CrearArray.java
@@ -0,0 +1,7 @@
+public class CrearArray {
+    public static void main(String[] args) {
+        int[] numeros = {10, 20, 30, 40, 50};
+        System.out.println("Primero: " + numeros[0]);
+        System.out.println("Último: " + numeros[4]);
+    }
+}
