4e16de8dd6020ef328e55cb21926ee4af4b9cc05
NTRJ91 <reyesjimeneznestor@gmail.com>
2026-06-05 13:51:40 +0200

    Create Perro.java

---
+++ java /practica-propia/Ejercicio 24: ClasePerro/Perro.java
@@ -0,0 +1,13 @@
+public class Perro {
+    private String nombre;
+    private int edad;
+
+    public Perro(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public void ladrar() {
+        System.out.println("Guau guau");
+    }
+}
