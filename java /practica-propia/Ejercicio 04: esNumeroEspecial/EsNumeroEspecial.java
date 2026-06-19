46dde519d35fb7834042dcec266730f56a7c45ae
NTRJ91 <reyesjimeneznestor@gmail.com>
2026-06-19 07:32:11 +0200

    Delete java /practica-propia/Ejercicio 04: esNumeroEspecial directory

--- java /practica-propia/Ejercicio 04: esNumeroEspecial/EsNumeroEspecial.java
+++
@@ -1,8 +0,0 @@
-public class EsNumeroEspecial {
-    public static boolean esNumeroEspecial(int numero) {
-        if (numero == 7) {
-            return false;
-        }
-        return numero >= 5 && numero <= 10;
-    }
-}
