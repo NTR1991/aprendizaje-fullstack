d35de41f7b2a9653d4dac6f99fdf3fcf6d67d20f
NTRJ91 <reyesjimeneznestor@gmail.com>
2026-06-19 07:28:29 +0200

    Add files via upload

---
+++ java /practica-propia/05-oop/Ejercicio 25_ ClaseGato/Gato.java
@@ -0,0 +1,13 @@
+public class Gato {
+    String nombre;
+    String color;
+
+    public Gato(String nombre, String color) {
+        this.nombre = nombre;
+        this.color = color;
+    }
+
+    public void maullar() {
+        System.out.println("Miau miau");
+    }
+}
