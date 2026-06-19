1da9c1ffb20606a776dd7003506c2846f5d202c6
NTRJ91 <reyesjimeneznestor@gmail.com>
2026-06-19 07:32:02 +0200

    Delete java /practica-propia/Ejercicio 03: puedeSubirMontanaRusa directory

--- java /practica-propia/Ejercicio 03: puedeSubirMontanaRusa/PuedeSubirMontanaRusa.java
+++
@@ -1,5 +0,0 @@
-public class PuedeSubirMontanaRusa {
-    public static boolean puedeSubirMontanaRusa(int altura, boolean tieneAcompanante) {
-        return altura >= 120 && (tieneAcompanante || altura >= 140);
-    }
-}
