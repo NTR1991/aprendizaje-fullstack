e33385cc3a776d8e7346a9b0adc6b82c65e9b111
NTRJ91 <reyesjimeneznestor@gmail.com>
2026-06-19 16:47:41 +0200

    Create 31_VariablesTypes.java

---
+++ java /exercises/01-fundamentals/31_VariablesTypes/31_VariablesTypes.java
@@ -0,0 +1,13 @@
+public class VariablesTypes {
+    public static void main(String[] args) {
+         int  edad= 25;
+         double altura = 1.75;
+         String  nombre = "Ana";
+         boolean  esEstudiante = true;
+
+        System.out.println(edad);
+        System.out.println(altura);
+        System.out.println(nombre);
+        System.out.println("Es estudiante: " + esEstudiante);
+    }
+}
