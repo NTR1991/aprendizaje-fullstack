b80980ca9d5e66dcbe3337528ecbdc5e75461766
NTRJ91 <reyesjimeneznestor@gmail.com>
2026-06-19 07:33:53 +0200

    Delete java /practica-propia/Ejercicio 16: recorrerArray directory

--- java /practica-propia/Ejercicio 16: recorrerArray/RecorrerArray.java
+++
@@ -1,8 +0,0 @@
-public class RecorrerArray {
-    public static void main(String[] args) {
-        int[] numeros = {10, 20, 30, 40, 50};
-        for (int i = 0; i < numeros.length; i++) {
-            System.out.println(numeros[i]);
-        }
-    }
-}
