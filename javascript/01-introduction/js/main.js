7d1610adbd9595a5d4e9654aee7cefd1331d4168
NTRJ91 <reyesjimeneznestor@gmail.com>
2026-06-23 17:17:46 +0200

    Delete javascript/01-introduction directory

--- javascript/01-introduction/js/main.js
+++
@@ -1,9 +0,0 @@
-console.log("jacinto");
-console.log(22);
-
-alert("Bienvenido");
-document.write("<p>Esto es js</p>");
-
-let curso = "Full Stack";
-console.log(curso);
-// Esta variable guarda el nombre del curso que estoy estudiando
